435bc6dde5399d22c8b06e90a8fe21254f6ee97b
gaby morgi <gabymorgi@gmail.com>
2022-12-01 19:43:17 -0300

    clase 20 updated

--- src/c20-localStorage/App.jsx
+++ src/c20-localStorage/App.jsx
@@ -1,32 +1,29 @@
-import React from "react";
-import './App.css';
-import { TheBag } from "./TheBag";
+import React from 'react'
+import './App.css'
+import { TheBag } from './TheBag'
 
 const App = () => {
-  //get theme from local storage
-  const theme = sessionStorage.getItem("theme");
-  //set theme to light if there is no theme in local storage
-  const [currentTheme, setCurrentTheme] = React.useState(theme || "light");
+  const [currentTheme, setCurrentTheme] = React.useState(
+    localStorage.getItem('theme') || 'dark'
+  )
 
   const toggleTheme = () => {
-    //if current theme is light, set theme to dark
-    if (currentTheme === "light") {
-      setCurrentTheme("dark");
-      sessionStorage.setItem("theme", "dark");
+    if (currentTheme === 'light') {
+      setCurrentTheme('dark')
+      localStorage.setItem('theme', 'dark')
     } else {
-      //if current theme is dark, set theme to light
-      setCurrentTheme("light");
-      sessionStorage.setItem("theme", "light");
+      setCurrentTheme('light')
+      localStorage.setItem('theme', 'light')
     }
-  };
+  }
 
   return (
-    <div className={theme || 'light'}>
+    <div className={currentTheme}>
       <h1>Local Storage</h1>
       <button onClick={toggleTheme}>Toggle Theme</button>
       <TheBag />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/c20-localStorage/TheBag.jsx
+++ src/c20-localStorage/TheBag.jsx
@@ -1,80 +1,134 @@
-import React from "react";
+import React from 'react'
 
-const actionType = {
-  ADD_START: "ADD_START",
-  ADD_END: "ADD_END",
-  REMOVE_START: "REMOVE_START",
-  REMOVE_END: "REMOVE_END",
-  RESET: "RESET",
-};
+//forma cutre de generar un id unico
+let id = 0
+
+function getUniqueKey() {
+  return id++
+}
+
+//opcional: normalizamos los tipos de acciones
+const actionTypes = {
+  addAlInicio: 'addAlInicio',
+  addAlFinal: 'addAlFinal',
+  eliminarPrimero: 'eliminarPrimero',
+  eliminarUltimo: 'eliminarUltimo',
+  reset: 'reset',
+}
+
+//operacion que calcula el valor inicial
+//recibe el segundo param del useReducer
+function init(initialValue) {
+  return localStorage.getItem('bag')
+    ? [JSON.parse(localStorage.getItem('bag'))]
+    : initialValue
+}
 
 /**
- * @param state array
- * @param action object with type and value properties
- * @returns the new state
+ * @param state el estado antes del setState
+ * @param action un objetito con las key type y payload
  */
-const reducer = (state, action) => {
+function bagReducer(state, action) {
+  //logica comun para todos los casos
+  let newState = [...state]
+  //evaluamos el tipo de accion que se esta ejecutando
   switch (action.type) {
-    case actionType.ADD_START:
-      return [action.value, ...state]
-    case actionType.ADD_END:
-      return [...state, action.value]
-    case actionType.REMOVE_START:
-      return state.slice(1)
-    case actionType.REMOVE_END:
-      return state.slice(0, -1)
-    case actionType.RESET:
-      return []
-    default:
-      throw new Error("Invalid action type")
+    case actionTypes.addAlInicio: {
+      const newElement = {
+        id: getUniqueKey(),
+        value: action.payload,
+      }
+      newState.unshift(newElement)
+      break
+    }
+    case actionTypes.addAlFinal: {
+      const newElement = {
+        id: getUniqueKey(),
+        value: action.payload,
+      }
+      newState.push(newElement)
+      break
+    }
+    case actionTypes.eliminarPrimero: {
+      newState.shift()
+      break
+    }
+    case actionTypes.eliminarUltimo: {
+      newState.pop()
+      break
+    }
+    case actionTypes.reset: {
+      newState = []
+      break
+    }
+    default: {
+      throw new Error(`No se reconoce el type: ${action.type}`)
+    }
   }
+  //logica comun para todos los casos
+  localStorage.setItem('bag', JSON.stringify(newState))
+  return newState
 }
 
 export const TheBag = () => {
-  const [value, setValue] = React.useState("");
-  const [bag, dispatchBag] = React.useReducer(reducer, []);
+  const [value, setValue] = React.useState('')
+  const [bag, dispatchBag] = React.useReducer(
+    bagReducer, //funcion que se ejecuta cuando se hace un dispatch, retorna el nuevo estado
+    [{ id: 100, value: 'manzana' }], //valor inicial
+    init //funcion que calcula el valor inicial
+  )
 
   const handleChange = (e) => {
-    setValue(e.target.value);
-  };
+    setValue(e.target.value)
+  }
 
-  const addAtStart = () => {
-    dispatchBag({ type: actionType.ADD_START, value });
-    setValue("");
-  };
+  const AgregarManzana = () => {
+    dispatchBag({ type: actionTypes.addAlInicio, payload: 'Manzana' })
+  }
 
-  const addAtEnd = () => {
-    dispatchBag({ type: actionType.ADD_END, value });
-    setValue("");
-  };
+  const AgregarBanana = () => {
+    dispatchBag({ type: actionTypes.addAlFinal, payload: 'Banana' })
+  }
 
-  const removeAtStart = () => {
-    dispatchBag({ type: actionType.REMOVE_START, value });
-  };
+  const AgregarAlFinal = () => {
+    //somos agnosticos a como se maneja el estado
+    //solo sabemos que queremos agregar un elemento al final
+    dispatchBag({ type: actionTypes.addAlFinal, payload: value })
+    setValue('')
+  }
 
-  const removeAtEnd = () => {
-    dispatchBag({ type: actionType.REMOVE_END, value });
-  };
+  const AgregarAlInicio = () => {
+    dispatchBag({ type: actionTypes.addAlInicio, payload: value })
+    setValue('')
+  }
+
+  const eliminarPrimero = () => {
+    dispatchBag({ type: actionTypes.eliminarPrimero })
+  }
+
+  const eliminarUltimo = () => {
+    dispatchBag({ type: actionTypes.eliminarUltimo })
+  }
 
   const resetBag = () => {
-    dispatchBag({ type: actionType.RESET, value });
-  };
+    dispatchBag({ type: actionTypes.reset })
+  }
 
   return (
-    <div>
-      <input
-        type="string"
-        value={value}
-        onChange={handleChange}
-      />
-      <button onClick={addAtStart}>Agregar al Inicio</button>
-      <button onClick={addAtEnd}>Agregar al Final</button>
-      <button onClick={removeAtStart}>Eliminar del Inicio</button>
-      <button onClick={removeAtEnd}>Eliminar del Final</button>
-      <button onClick={resetBag}>Reset!!!</button>
-      {bag.map((item, index) => (
-        <div key={index}>{item}</div>
+    <div className='card flex-column'>
+      Random List
+      <input type='string' value={value} onChange={handleChange} />
+      <hr />
+      <button onClick={AgregarManzana}>Agregar manzana</button>
+      <button onClick={AgregarBanana}>Agregar banana</button>
+      <button onClick={AgregarAlInicio}>Agregar al inicio</button>
+      <button onClick={AgregarAlFinal}>Agregar al final</button>
+      <button onClick={eliminarPrimero}>Elimine el primer Elemento</button>
+      <button onClick={eliminarUltimo}>Elimine el ultimo Elemento</button>
+      <button onClick={resetBag}>Reset</button>
+      {bag.map((item) => (
+        <div key={item.id}>{item.value}</div>
       ))}
     </div>
-  );
-};
+  )
+}
