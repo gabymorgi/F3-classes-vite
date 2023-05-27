fe1c4ef2c1e70ba9b1f3d58f0b0bcbb9de4bc170
gaby morgi <gabymorgi@gmail.com>
2023-05-26 22:24:35 -0300

    enunciado repaso

---
+++ src/clases/10-repaso/enunciado.md
@@ -0,0 +1,21 @@
+# Pedi3 ya
+
+Vamos a crear una applicacion que gestione una lista de pedidos de un restaurante.
+
+## Funcionalidades
+
+1. Crear un pedido
+
+La app debera contener un formulario que permita crear un pedido. El pedido debera tener los siguientes campos:
+
+- plato: string
+
+Al hacer click en el boton de crear, se debera crear un pedido con los datos ingresados y agregarlo a la lista de pedidos.
+
+2. Listar pedidos
+
+La app debera mostrar una lista de todos los pedidos creados.
+
+3. Componente pedido
+
+El componente pedido debera mostrar el plato del pedido y un boton para eliminarlo.
--- src/sandbox/App.jsx
+++ src/sandbox/App.jsx
@@ -1,48 +1,79 @@
-import { useState } from 'react'
-import Menu from './components/Menu'
-import Pedido from './components/Pedido'
-
-// necesito generar un id unico y que no cambie
-let uniqueId = 0
+import { useState } from "react"
 
 const App = () => {
-  const [pedidos, setPedidos] = useState([])
+  const [name, setName] = useState('')
+  const [edad, setEdad] = useState(0)
 
-  function handleSubmit(value) {
-    console.log('submit', value)
-    const newPedido = {
-      id: uniqueId++,
-      nombre: value,
+  const [submitedValues, setSubmitedValues] = useState()
+  const [error, setError] = useState()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log('Form submitted', name)
+    // validations
+    if (name === "") {
+      console.error('Name is required')
+      setError('Name is required')
+      return
+    } else {
+      // aca esta todo bien
+      setError()
+      // enviar informacion
+      setSubmitedValues({
+        name,
+        edad,
+      })
+      setName('')
+      setEdad('')
     }
-    setPedidos([newPedido, ...pedidos])
   }
 
-  function handleDelete(id) {
-    console.log('click')
-    const newPedidos = pedidos.filter((pedido) => pedido.id !== id)
-    setPedidos(newPedidos)
+  console.log("render", error)
+
+  function handleChangeNombre(e) {
+    console.log(e.target.value)
+    // parser
+    setName(e.target.value.toLowerCase())
+  }
+
+  function handleChangeEdad(e) {
+    console.log(e.target.value)
+    // parser
+    setEdad(e.target.value)
   }
 
   return (
     <div className='flex-col'>
-      <Menu
-        onSubmit={handleSubmit}
-      >
-        <p>adsfasd</p>
-      </Menu>
-      <hr />
-      <ul>
-        {pedidos.map((pedido) => {
-          return (
-            <li key={pedido.id}>
-              <Pedido
-                nombre={pedido.nombre}
-                onClick={() => handleDelete(pedido.id)}
-              />
-            </li>
-          )
-        })}
-      </ul>
+      Formulario
+      <form onSubmit={handleSubmit}>
+        <div className="flex">
+          <label htmlFor='name'>Name: </label>
+          <input
+            id="name"
+            placeholder="John Doe"
+            value={name}
+            onChange={handleChangeNombre}
+          />
+        </div>
+        <div className="flex">
+          <label htmlFor='edad'>Edad: </label>
+          <input
+            id="edad"
+            placeholder="33"
+            type="number"
+            value={edad}
+            onChange={handleChangeEdad}
+          />
+        </div>
+        {error ? <p>{error}</p> : undefined}
+        <button type="submit">Submit</button>
+      </form>
+      {submitedValues ? (
+        <div>
+          <p>Nombre: {submitedValues.name}</p>
+          <p>Edad: {submitedValues.edad}</p>
+        </div>
+      ) : undefined}
     </div>
   )
 }
